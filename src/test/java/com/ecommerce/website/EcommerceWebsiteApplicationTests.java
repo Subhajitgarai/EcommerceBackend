9dfd23383d212994a25b2bc5a5a16785674ac57f
Subhajitgarai <subhajitgarai12345@gmail.com>
2024-02-26 10:16:47 +0530

    Ecommerce Website Base Uploaded

---
+++ src/test/java/com/ecommerce/website/EcommerceWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
